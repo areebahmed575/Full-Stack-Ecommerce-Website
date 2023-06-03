93fa827ac2ec91f394ba28f382425faed6dc43d0
areebahmed575 <areebahmed575@gmail.com>
2023-06-03 14:31:22 +0500

    solved some deployment error of wrapper

--- app/components/Wrapper.tsx
+++ app/components/Wrapper.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { FC } from 'react'
-const Wrapper: FC<{childern:React.ReactNode}>= ({children}) => {
+const Wrapper: FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <div className='max-w-7xl   px-3 md:px-10 mx-auto'>
-        {children}
+      {children}
 
-        
-      
+
+
     </div>
   )
 }
